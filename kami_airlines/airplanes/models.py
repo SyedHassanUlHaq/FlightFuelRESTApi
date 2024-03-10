916016ea267ba14ea91506aedf647a3c910b7e76
SyedHassanUlHaq <21b-029-cs@students.uit.edu>
2024-03-10 18:57:58 +0500

    created Airplane model

--- kami_airlines/airplanes/models.py
+++ kami_airlines/airplanes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from math import log
+
+class Airplane(models.Model):
+    airplane_id = models.IntegerField(unique=True)
+    passengers = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Airplane {self.airplane_id} with {self.passengers} passengers"
+
+    @property
+    def fuel_tank_capacity(self):
+        """Calculate fuel tank capacity based on airplane ID."""
+        return 200 * self.airplane_id
+
+    @property
+    def fuel_consumption_per_minute(self):
+        """Calculate base fuel consumption per minute, not accounting for passengers."""
+        return log(self.airplane_id) * 0.80
